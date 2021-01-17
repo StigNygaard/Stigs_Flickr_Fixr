5165a8c0a24ac29414500968759955d87caded6c
Stig Nygaard <stig@rockland.dk>
2021-01-17 17:56:19 +0100

    Also try xIFr...

--- WebExtension/onboard/onboard.js
+++ WebExtension/onboard/onboard.js
@@ -1,5 +1,9 @@
+function isFirefox() {
+  return !!((typeof browser !== 'undefined') && browser.runtime && browser.runtime.getURL("./").startsWith("moz-extension://"));
+}
 function init() {
-  document.getElementById('verstr').textContent = browser.runtime.getManifest().version;
-  document.getElementById('settings').addEventListener('click', () => browser.runtime.openOptionsPage());
+  if (document.getElementById('verstr')) document.getElementById('verstr').textContent = browser.runtime.getManifest().version;
+  if (document.getElementById('settings')) document.getElementById('settings').addEventListener('click', () => browser.runtime.openOptionsPage());
+  if (document.getElementById('ffo') && isFirefox()) document.getElementById('ffo').style.display = 'block';
 }
 window.addEventListener('DOMContentLoaded', init);
