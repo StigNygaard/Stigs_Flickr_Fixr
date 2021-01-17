04cb574b0dc60e5d8e4fe498c4971e866f802e20
Stig Nygaard <stig@rockland.dk>
2021-01-17 17:51:16 +0100

    versionnumber IIFE

--- WebExtension/background/flickr_fixr.js
+++ WebExtension/background/flickr_fixr.js
@@ -1,20 +1,20 @@
-var versionnumber = { // major.minor.revision
-    current: function () {
+var versionnumber = (function Versionnumber() {  // major.minor.revision
+    function current() {
         if (browser && browser.runtime) { // webextension versionnumber
             return browser.runtime.getManifest().version;
         }
         // undefined
-    },
-    compare: function (v1, v2) {
+    }
+    function compare(v1, v2) {
         if (typeof v2 === 'undefined') {
             v2 = v1;
-            v1 = versionnumber.current();
+            v1 = current();
         }
-        var v1parts = v1.split('.');
-        var v2parts = v2.split('.');
+        let v1parts = v1.split('.');
+        let v2parts = v2.split('.');
         while (v1parts.length < v2parts.length) v1parts.push(0);
         while (v2parts.length < v1parts.length) v2parts.push(0);
-        for (var i = 0; i < v1parts.length; ++i) {
+        for (let i = 0; i < v1parts.length; ++i) {
             if (parseInt(v1parts[i], 10) > parseInt(v2parts[i], 10)) {
                 return 1;
             } else if (parseInt(v1parts[i], 10) < parseInt(v2parts[i], 10)) {
@@ -22,36 +22,46 @@ var versionnumber = { // major.minor.revision
             }
         }
         return 0;
-    },
-    parts: function (v, n) {
+    }
+    function parts(v, n) {
         if (typeof n === 'undefined') {
             n = v;
-            v = versionnumber.current();
+            v = current();
         }
-        var vparts = v.split('.', n);
+        let vparts = v.split('.', n);
         vparts.map( function(item) {return String(parseInt(item.trim(),10))});
         while (vparts.length < n) vparts.push('0');
         return vparts.join('.');
-    },
-    major: function (v) {
-        if (typeof v === 'undefined') {
-            v = versionnumber.current();
-        }
-        return versionnumber.parts(v, 1);
-    },
-    minor: function (v) {
-        if (typeof v === 'undefined') {
-            v = versionnumber.current();
-        }
-        return versionnumber.parts(v, 2);
-    },
-    revision: function (v) {
-        if (typeof v === 'undefined') {
-            v = versionnumber.current();
-        }
-        return versionnumber.parts(v, 3);
     }
-};
+    function major(v) {
+        if (typeof v === 'undefined') {
+            v = current();
+        }
+        return parts(v, 1);
+    }
+    function minor(v) {
+        if (typeof v === 'undefined') {
+            v = current();
+        }
+        return parts(v, 2);
+    }
+    function revision(v) {
+        if (typeof v === 'undefined') {
+            v = current();
+        }
+        return parts(v, 3);
+    }
+
+    // API:
+    return {
+        current,
+        compare,
+        major,
+        minor,
+        revision
+    };
+
+})();
 
 function messageHandler(request, sender, sendResponse) {
     // console.log("Message received from the content script: " + JSON.stringify(request));
