836827b9d2834ee106601ac5d96cbaf154cf9072
Stig Nygaard <stig@rockland.dk>
2021-01-17 18:26:01 +0100

    userscript vers. 2021.01.17.0

--- WebExtension/Stigs_Flickr_Fixr.user.js
+++ WebExtension/Stigs_Flickr_Fixr.user.js
@@ -14,7 +14,7 @@
 // @exclude     *://*.flickr.com/signin/*
 // @exclude     *://*.flickr.com/signup/*
 // @exclude     *://*.flickr.com/account/*
-// @version     2020.06.22.0
+// @version     2021.01.17.0
 // @run-at      document-start
 // @grant       none
 // @noframes
