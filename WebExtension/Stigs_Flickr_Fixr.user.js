ba346288442d079536d47abf5a9b67f3e6ef3f7c
Stig Nygaard <stig@rockland.dk>
2023-07-20 17:52:38 +0200

    Add Notifications to You menu

--- WebExtension/Stigs_Flickr_Fixr.user.js
+++ WebExtension/Stigs_Flickr_Fixr.user.js
@@ -530,6 +530,11 @@ function topMenuItems() {
                 gid.insertAdjacentElement('afterend', createRichElement('li', {
                     class: 'menuitem',
                     role: 'menuitem',
+                    'aria-label': 'Notifications'
+                }, createRichElement('a', {'data-track': 'gnYouNotificationsClick', href: '/notifications'}, 'Notifications')));
+                gid.insertAdjacentElement('afterend', createRichElement('li', {
+                    class: 'menuitem',
+                    role: 'menuitem',
                     'aria-label': 'Tags'
                 }, createRichElement('a', {'data-track': 'gnYouTagsClick', href: '/photos/me/tags'}, 'Tags')));
                 aad.parentElement.insertAdjacentElement('afterend', createRichElement('li', {
@@ -547,6 +552,10 @@ function topMenuItems() {
                 }, 'Collections')));
             } else if (gid.classList.contains('gn-subnav-item') && !m.querySelector('a[data-track=You-tags]')) {
                 // earlier design
+                gid.insertAdjacentElement('afterend', createRichElement('li', {class: 'gn-subnav-item'}, createRichElement('a', {
+                    'data-track': 'You-notifications',
+                    href: '/notifications'
+                }, 'Notifications')));
                 gid.insertAdjacentElement('afterend', createRichElement('li', {class: 'gn-subnav-item'}, createRichElement('a', {
                     'data-track': 'You-tags',
                     href: '/photos/me/tags'
