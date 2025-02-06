38b9a50f31d24a2c6a4274fe5f548882044fc991
Stig Nygaard <stig@rockland.dk>
2025-02-06 20:43:08 +0100

    textarea dynamic height

--- WebExtension/Stigs_Flickr_Fixr.user.js
+++ WebExtension/Stigs_Flickr_Fixr.user.js
@@ -850,8 +850,8 @@ function albumTeaserDelayed() {
     _timerAlbumTeaserDelayed = setTimeout(albumTeaser, 1500);
 }
 
-const resizeableCommenting_style_vert= '.fixr-ta-resizeable {margin:0;width:100%;font-weight:400} .group-discussion-topic-view .post textarea, section.add-comment-section .fixr-ta-resizeable textarea {resize:vertical;width:100%;min-height:6em;max-width:100%}';
-const resizeableCommenting_style_both= '.fixr-ta-resizeable {margin:0;width:100%;font-weight:400} .group-discussion-topic-view .post textarea, section.add-comment-section .fixr-ta-resizeable textarea {resize:both;width:100%;min-height:6em;max-width:100%}';
+const resizeableCommenting_style_vert= '.fixr-ta-resizeable {margin:0;width:100%;font-weight:400} .group-discussion-topic-view .post textarea, section.add-comment-section .fixr-ta-resizeable textarea {resize:vertical;width:100%;min-height:6em;max-width:100%;field-sizing:content}';
+const resizeableCommenting_style_both= '.fixr-ta-resizeable {margin:0;width:100%;font-weight:400} .group-discussion-topic-view .post textarea, section.add-comment-section .fixr-ta-resizeable textarea {resize:both;width:100%;min-height:6em;max-width:100%;field-sizing:content}';
 // TODO styling "speech bubbles" style?:  https://www.smashingmagazine.com/2024/03/modern-css-tooltips-speech-bubbles-part1/
 function resizeableCommenting() {
     const selector = '.fluid section.add-comment-section div.text-area-wrapper';
